bcc45defd4baa54796f4ebcc3a985fed51b46167
Guillermo Antonio Ortiz Corella <gaoc117@gmail.com>
2023-12-19 23:05:29 -0600

    x
    x

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+const { src, dest } = require('gulp');
+const sass = require('gulp-sass')(require('sass')); 
+
+function css(done) {
+    
+    src('scr/scss/app.scss') //identificar el archivo principal
+    .pipe(sass()) //Compilar SASS
+    .pipe(dest('build/css')) //exportarlo o guardarlo en uuna ubicacion
+
+
+
+    
+    done();
+}
+
+exports.css = css;
