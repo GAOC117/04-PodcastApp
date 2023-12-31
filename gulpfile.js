71d9d6fbf74cd7e6434dac8cbdbc78358959d8c3
Guillermo Antonio Ortiz Corella <gaoc117@gmail.com>
2023-12-30 22:22:22 -0600

    x
    x

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,17 @@
-const { src, dest } = require('gulp');
+const { src, dest, watch } = require('gulp');
 const sass = require('gulp-sass')(require('sass')); 
 
 function css(done) {
-    
     src('src/scss/app.scss') //identificar el archivo principal
     .pipe(sass()) //Compilar SASS
     .pipe(dest('build/css')) //exportarlo o guardarlo en uuna ubicacion
-
-
-
-    
     done();
 }
 
+function dev(){
+    //watch('si hay cambios en este archivo', llama esta funcion)
+    watch('src/scss/app.scss',css);
+}
+
 exports.css = css;
+exports.dev = dev;
