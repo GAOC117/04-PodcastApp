95c73665ceb015ba2dd3978164e59b8772dcc178
Guillermo Antonio Ortiz Corella <gaoc117@gmail.com>
2024-01-07 21:43:12 -0600

    y
    y

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,30 @@
-const { src, dest, watch } = require('gulp');
-const sass = require('gulp-sass')(require('sass')); 
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
 
-function css(done) {
-    src('src/scss/app.scss') //identificar el archivo principal
-    .pipe(sass()) //Compilar SASS
-    .pipe(dest('build/css')) //exportarlo o guardarlo en uuna ubicacion
+//Imagenes
+const imagemin = require("gulp-imagemin");
+
+
+function css(done){
+    src("src/scss/app.scss")//Identificar el archivo principal de la hoja de estilos
+    .pipe(sass())//Compilar sass
+    .pipe(dest("build/css"));//Exportar o guardarlo en una ubicación
     done();
 }
 
+
 function dev(){
-    //watch('si hay cambios en este archivo', llama esta funcion)
-    watch('src/scss/app.scss',css);
+    watch("src/scss/**/*.scss",css); //revisa que archivo es el que cambia y manda a llamar la que genera todo
 }
 
-exports.css = css;
-exports.dev = dev;
+function imagenes(done){
+    src("src/img/**/*") //sin extension busca todos los formatos
+    .pipe(imagemin({optimizationLevel: 3}))
+    .pipe(dest("build/img"));
+    done();
+}
+
+exports.css = css; //escribiendo gulp css
+exports.dev = dev; //escribiendo gulp dev
+exports.imagenes = imagenes; //escribiendo gulp imagenes
+exports.default = series(imagenes, css, dev); //aqui solo con escribir gulp
